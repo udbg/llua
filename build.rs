ce2d476e54209c6aacf4023f545d2810e8528faa
metaworm <metaworm@outlook.com>
2022-06-13 23:17:52 +0800

    improve build

--- build.rs
+++ build.rs
@@ -1,6 +1,9 @@
 #[cfg(feature = "vendored")]
 fn main() {
     use std::env;
+    use std::path::Path;
+
+    const LUA_DIR_NAME: &str = "lua-5.4.4";
 
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
     let target_family = env::var("CARGO_CFG_TARGET_FAMILY").unwrap();
@@ -19,12 +22,18 @@ fn main() {
     if cfg!(debug_assertions) {
         config.define("LUA_USE_APICHECK", None);
     }
-    println!("cargo:rerun-if-changed=lua-5.4.4/");
+    println!("cargo:rerun-if-changed={LUA_DIR_NAME}/");
+    println!(
+        "cargo:luasrc={}",
+        Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap())
+            .join(LUA_DIR_NAME)
+            .to_string_lossy()
+    );
 
     if env::var("CARGO_FEATURE_THREAD").is_ok() {
         config.define("LUA_USER_H", "\"../src/llua.h\"");
     }
-    add_files(&mut config, "lua-5.4.4", |n| {
+    add_files(&mut config, LUA_DIR_NAME, |n| {
         n.ends_with(".c") && !n.ends_with("lua.c") && !n.ends_with("luac.c")
     });
     config.compile("lua54");
@@ -40,7 +49,7 @@ fn main() {
 
     fn lua_builder() -> cc::Build {
         let mut result = cc::Build::new();
-        result.include("lua-5.4.4");
+        result.include(LUA_DIR_NAME);
         result
     }
 }
